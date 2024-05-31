773a46ad2f757a551465ce98320c50bf0c34b126
RosaFraile <44233739+RosaFraile@users.noreply.github.com>
2024-05-31 21:19:53 +0200

    Update env.js

--- env.js
+++ env.js
@@ -2,7 +2,9 @@ module.exports = {
   devServer: {
     host: '0.0.0.0',
     disableHostCheck: true,
-    allowedHosts: 'all',
+    allowedHosts: [
+      'https://rosa-fraile-react-portfolio.onrender.com'
+    ],
     port: '3000'
   }
 };
